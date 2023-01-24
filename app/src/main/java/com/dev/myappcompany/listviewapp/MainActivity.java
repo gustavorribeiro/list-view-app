80cd71216d2c667513edda99c84721098d9ee4dd
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-24 15:05:08 -0300

    adding list and onClick items logger

--- app/src/main/java/com/dev/myappcompany/listviewapp/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/listviewapp/MainActivity.java
@@ -3,6 +3,13 @@ package com.dev.myappcompany.listviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        final ArrayList<String> myAppleDevices = new ArrayList<String>();
+
+        myAppleDevices.add("MacBook Air");
+        myAppleDevices.add("MacBook Pro");
+        myAppleDevices.add("Iphone 13 Pro");
+        myAppleDevices.add("AirPods 3");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myAppleDevices);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("Device selected:", myAppleDevices.get(i));
+            }
+        });
+
     }
 }
